b48cdd48983778c08d1313ef0e2dd4285a543888
Balint Kemeny <52007126+balintkemeny@users.noreply.github.com>
2023-05-20 09:59:54 +0200

    Part1a: Add data to components via props

--- part1/app/src/App.js
+++ part1/app/src/App.js
@@ -1,13 +1,18 @@
-const Hello = () => (
-  <div>
-    <p>Hello, World!</p>
-  </div>
-)
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello, {props.name}!</p>
+      <p>Your are {props.age} years old.</p>
+    </div>
+  )
+}
 
 const App = () => (
   <div>
     <h1>Greetings!</h1>
-    <Hello />
+    <Hello name='George' age = {2 * 16} />
+    <Hello name='Daisy'  age = {5} />
   </div>
 )
 
