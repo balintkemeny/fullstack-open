28ea6670b58a9ef80713d48d1761abcefb9874e4
Balint Kemeny <52007126+balintkemeny@users.noreply.github.com>
2023-05-27 21:29:09 +0200

    Part1c: Destructuring practice

--- part1/practice/app/src/App.js
+++ part1/practice/app/src/App.js
@@ -1,19 +1,27 @@
-const Hello = (props) => {
-  console.log(props)
+const Hello = ({ name, age }) => {
+  const yearOfBirth = () => new Date().getFullYear() - age
+  
   return (
     <div>
-      <p>Hello, {props.name}!</p>
-      <p>Your are {props.age} years old.</p>
+      <p>Hello, {name}!</p>
+      <p>You are {age} years old.</p>
+      <p>So you were probably born in {yearOfBirth()}.</p>
     </div>
   )
 }
 
-const App = () => (
-  <div>
-    <h1>Greetings!</h1>
-    <Hello name='George' age = {2 * 16} />
-    <Hello name='Daisy'  age = {5} />
-  </div>
-)
+const App = () => {
+  const name = 'Peter'
+  const age = 25
+  
+  return (
+    <div>
+      <h1>Greetings!</h1>
+      <Hello name='George' age = {2 * 16} />
+      <Hello name='Daisy'  age = {5} />
+      <Hello name= {name}  age = {age} />
+    </div>
+  )
+}
 
 export default App
---
+++ part1/practice/js-practice/methods.js
@@ -0,0 +1,2 @@
+//In React, we are going to use React Hooks, so defining methods on objects is not very relevant in this context
+
