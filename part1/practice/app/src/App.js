4e3f4321215816efb0a1c19fbde753a54ad53931
Balint Kemeny <52007126+balintkemeny@users.noreply.github.com>
2023-05-28 14:51:39 +0200

    Part1c: Event handler functions (on-click)

--- part1/practice/app/src/App.js
+++ part1/practice/app/src/App.js
@@ -3,11 +3,17 @@ import { useState } from "react"
 const App = () => {
   const [ counter, setCounter ] = useState(0)
   
+  const incrementCounter = () => setCounter(counter + 1)
+  const resetCounterToZero = () => setCounter(0)
+
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
+      <button onClick={incrementCounter}>
         plus
+      </button>
+      <button onClick={resetCounterToZero}>
+        reset
       </button>
     </div>
   )
