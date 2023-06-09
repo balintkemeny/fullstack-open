7be50d57f73ab71d4e226830e5b89f88627301e3
Balint Kemeny <52007126+balintkemeny@users.noreply.github.com>
2023-06-09 10:26:36 +0200

    Part1d: Complex state - arrays

--- part1/practice/app/src/App.js
+++ part1/practice/app/src/App.js
@@ -1,36 +1,31 @@
 import { useState } from "react"
 
 const App = () => {
-  const [ clicks, setClicks ] = useState(
-    {left: 0, right: 0}
-  );
+  const [ left, setLeft ] = useState(0);
+  const [ right, setRight ] = useState(0);
+  const [ allClicks, setAllClicks ] = useState([]);
 
   const handleLeftClick = () => {
-    const newClicks = {
-      ...clicks,
-      left: clicks.left + 1,
-    };
-    setClicks(newClicks);
+    setLeft(left + 1)
+    setAllClicks(allClicks.concat('L'))
   };
 
   const handleRightClick = () => {
-    const newClicks = {
-      ...clicks,
-      right: clicks.right + 1
-    };
-    setClicks(newClicks);
+    setRight(right + 1)
+    setAllClicks(allClicks.concat('R'))
   };
 
   return (
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeftClick}>
         left
       </button>
       <button onClick={handleRightClick}>
         right
       </button>
-      {clicks.right}
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
