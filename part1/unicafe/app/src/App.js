b2b2570bc6c0518c73c08514a3ffd15607f966a9
Balint Kemeny <52007126+balintkemeny@users.noreply.github.com>
2023-06-09 15:30:32 +0200

    Ex1.8: unicafe step 3

--- part1/unicafe/app/src/App.js
+++ part1/unicafe/app/src/App.js
@@ -10,26 +10,15 @@ const Display = ({ label, value }) => (
   <div>{label} {value}</div>
 )
 
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad
-
-  const incrementGood = () => setGood(good + 1)
-  const incrementNeutral = () => setNeutral(neutral + 1)
-  const incrementBad = () => setBad(bad + 1)
+  
   const getAverage = () => (good - bad) / total
   const getPositivePercentage = () => good / total * 100
   const getPositivePercentageString = () => getPositivePercentage().toString().concat('%')
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button handleClick={incrementGood} text='good' />
-      <Button handleClick={incrementNeutral} text='neutral' />
-      <Button handleClick={incrementBad} text='bad' />
       <h1>statistics</h1>
       <Display label='good' value={good} />
       <Display label='neutral' value={neutral} />
@@ -37,6 +26,26 @@ const App = () => {
       <Display label='all' value={total} />
       <Display label='average' value={getAverage()} />
       <Display label='positive' value={getPositivePercentageString()} />
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const incrementGood = () => setGood(good + 1)
+  const incrementNeutral = () => setNeutral(neutral + 1)
+  const incrementBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={incrementGood} text='good' />
+      <Button handleClick={incrementNeutral} text='neutral' />
+      <Button handleClick={incrementBad} text='bad' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
