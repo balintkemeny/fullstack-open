b30605010c25883c6c5ea58c3bc877079b41d4d4
Balint Kemeny <52007126+balintkemeny@users.noreply.github.com>
2023-06-16 14:51:20 +0200

    Ex2.8: The Phonebook step 3

--- part2/phonebook/app/src/App.js
+++ part2/phonebook/app/src/App.js
@@ -2,41 +2,52 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas' }
+    { id: 1, name: 'Arto Hellas', number: '+151-2233445' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const handleInputChange = (event) => {
-    console.log(event.target.value)
+  const handleInputChangeName = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleInputChangeNumber = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addNote = (event) => {
     event.preventDefault()
+    
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to the Phonebook!`)
+      setNewName('')
+      setNewNumber('')
+      return
+    }
+    
     const contactObject = {
       id: persons.length + 1,
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     console.log('Button clicked. New contact added: ', contactObject)
     setPersons(persons.concat(contactObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNote}>
-        <div>
-          name: <input onChange={handleInputChange} value={newName}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input onChange={handleInputChangeName} value={newName}/></div>
+        <div>number: <input onChange={handleInputChangeNumber} value={newNumber}/></div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.id}>{person.name}</li>)}
+        {persons.map(person => <li key={person.id}>{person.name}: {person.number}</li>)}
       </ul>
     </div>
   )
