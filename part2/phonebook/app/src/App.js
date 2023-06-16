f4551ed1fcba9b919f131281438526bf2e65a257
Balint Kemeny <52007126+balintkemeny@users.noreply.github.com>
2023-06-16 15:22:23 +0200

    Ex2.9: The Phonebook step 4

--- part2/phonebook/app/src/App.js
+++ part2/phonebook/app/src/App.js
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
+  const [contacts, setContacts] = useState([
     { id: 1, name: 'Arto Hellas', number: '+151-2233445' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [nameFilter, setNameFilter] = useState('')
+
+  const contactsToDisplay = nameFilter === ''
+    ? contacts
+    : contacts.filter(contact => contact.name.toLowerCase().includes(nameFilter.toLowerCase()))
 
   const handleInputChangeName = (event) => {
     setNewName(event.target.value)
@@ -18,7 +23,7 @@ const App = () => {
   const addNote = (event) => {
     event.preventDefault()
     
-    if (persons.some(person => person.name === newName)) {
+    if (contacts.some(person => person.name === newName)) {
       alert(`${newName} is already added to the Phonebook!`)
       setNewName('')
       setNewNumber('')
@@ -26,13 +31,13 @@ const App = () => {
     }
     
     const contactObject = {
-      id: persons.length + 1,
+      id: contacts.length + 1,
       name: newName,
       number: newNumber
     }
 
     console.log('Button clicked. New contact added: ', contactObject)
-    setPersons(persons.concat(contactObject))
+    setContacts(contacts.concat(contactObject))
     setNewName('')
     setNewNumber('')
   }
@@ -40,14 +45,18 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>search: <input onChange={(event) => setNameFilter(event.target.value)} value={nameFilter}/></div>
+      </form>
+      <h2>Add a new contact:</h2>
       <form onSubmit={addNote}>
         <div>name: <input onChange={handleInputChangeName} value={newName}/></div>
         <div>number: <input onChange={handleInputChangeNumber} value={newNumber}/></div>
         <div><button type="submit">add</button></div>
       </form>
-      <h2>Numbers</h2>
+      <h2>Contacts:</h2>
       <ul>
-        {persons.map(person => <li key={person.id}>{person.name}: {person.number}</li>)}
+        {contactsToDisplay.map(person => <li key={person.id}>{person.name}: {person.number}</li>)}
       </ul>
     </div>
   )
